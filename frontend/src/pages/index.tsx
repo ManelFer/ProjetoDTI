9acb85e30b62748f133988600b0ee80a8885e889
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2024-12-16 13:51:12 -0300

    tentativa de auth
    tentativa de autenticação

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import Button from "@/components/Button";
+
+const AuthPage = () => {
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [isLogining, setIsLogiging] = useState(false);
+  const [form, setForm] = React.useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleRegister = async () => {
+    console.log(form);
+  };
+
+  const handleSubmit = async () => {
+    console.log(form);
+  };
+
+  return (
+    <div className="flex min-h-screen flex-1 flex-col justify-center bg-[#ffffff] px-6 py-12 text-white lg:px-8">
+      <Head>
+        <title>GZAPPY</title>
+      </Head>
+
+      <div className="mx-auto mt-8 w-full max-w-[388px] rounded bg-[#f2f2f2] p-3 text-black md:p-8">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight">
+          Fazer login na sua conta
+        </h2>
+
+        <div className="mt-8 flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="fullname"
+              className="block text-sm font-medium leading-6"
+            >
+              Seu Nome
+            </label>
+
+            <input
+              id="fullname"
+              name="fullname"
+              type="fullname"
+              autoComplete="fullname"
+              className="block w-full rounded-md border-0 p-3 py-1.5 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              value={form.fullname}
+              onChange={(e) => setForm({ ...form, fullname: e.target.value })}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6"
+            >
+              Seu email
+            </label>
+
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              className="block w-full rounded-md border-0 p-3 py-1.5 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6"
+            >
+              Sua senha
+            </label>
+
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              className="block w-full rounded-md border-0 p-3 py-1.5 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+              value={form.password}
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+            />
+          </div>
+
+          <div className="mt-3 flex gap-4">
+            <Button
+              label="Registrar"
+              variant="secondary"
+              onClick={handleRegister}
+              isLoading={isRegistering}
+              isDisabled={isRegistering}
+            />
+            <Button
+              label="Acessar"
+              variant="primary"
+              onClick={handleSubmit}
+              isLoading={isLogining}
+              isDisabled={isLogining}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
