5e6a9c3be081947939a66579ef0203b6abaad2cb
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2024-12-17 07:56:28 -0300

    apaguei
    apaguei a pasta backend

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('postgres', 'postgres', '1234', {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres',
-})
-module.exports = sequelize;
